0f5c102c683905c355683e49d1ab6c8573d81c57
Gift Chimphonda <gift.chimphonda@outlook.com>
2021-12-14 14:05:41 +0200

    Completed 1st Draft Blockchain

--- 5_blockchain.py
+++ 5_blockchain.py
@@ -1,10 +1,13 @@
 import hashlib
+import random
+from datetime import datetime
 
 
 def calc_hash(self):
     sha = hashlib.sha256()
 
-    hash_str = "We are going to encode this string of data!".encode('utf-8')
+    hash_str = f'{self.data}{self.timestamp}{self.previous_hash}'.encode(
+        'utf-8')
 
     sha.update(hash_str)
 
@@ -14,11 +17,15 @@ def calc_hash(self):
 class Block:
 
     def __init__(self, timestamp, data, previous_hash):
+        self.calc_hash = calc_hash
         self.timestamp = timestamp
         self.data = data
         self.previous_hash = previous_hash
-        self.hash = self.calc_hash()
+        self.hash = self.calc_hash(self)
         self.next = None
+
+    def __repr__(self) -> str:
+        return f'<--\n Timestamp: {self.timestamp}, \n Data: {self.data}, \n SHA 256 Hash: {self.hash}, \n Prev_Hash: {self.previous_hash} \n-->'
 
 
 class Blockchain(object):
@@ -26,8 +33,35 @@ class Blockchain(object):
         self.head = None
         self.tail = None
 
-    def insert(self, block):
-          if self.head is None:
-                block.previous_hash = 0
-                self.head = block
-                self.tail = self.head
+    def insert(self, data):
+        dt = datetime.now()
+        timestamp = datetime.timestamp(dt)
+        if self.head is None:
+            block = Block(timestamp=timestamp, data=data, previous_hash=0)
+            self.head = block
+            self.tail = self.head
+        else:
+            block = Block(timestamp=timestamp, data=data,
+                          previous_hash=self.tail.hash)
+            self.tail.next = block
+            self.tail = self.tail.next
+
+    def traverse(self):
+        node = self.head
+        while node is not None:
+            print(node)
+            node = node.next
+
+
+blockchain = Blockchain()
+sample = 'abcdefghijklmnopqrstuvwxyz'
+length = 20
+strings = [None for i in range(length)]
+
+for i in range(length):
+    strings[i] = ''.join(random.choice(sample) for i in range(length))
+
+for string in strings:
+    blockchain.insert(string)
+
+blockchain.traverse()
